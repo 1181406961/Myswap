afeaff87f61483c022f682d87309abd77899f1b2
1181406961 <1181406961@qq.com>
2022-12-31 15:29:27 +0800

    修改流动性数学部分计算

--- src/lib/LiquidityMath.sol
+++ src/lib/LiquidityMath.sol
@@ -44,20 +44,22 @@ library LiquidityMath {
 
     function getLiquidityForAmounts(
         uint160 sqrtPriceX96,
-        uint160 sqrtPriceAX96,
-        uint160 sqrtPriceBX96,
-        uint256 amount0,
-        uint256 amount1
+        uint160 sqrtPriceAX96, // 下界
+        uint160 sqrtPriceBX96, // 上界
+        uint256 amount0, // token 0
+        uint256 amount1 // token 1
     ) internal pure returns (uint128 liquidity) {
+        // 根据用户选择的price区间，price和两种token计算对应的liquidity
         if (sqrtPriceAX96 > sqrtPriceBX96)
             (sqrtPriceAX96, sqrtPriceBX96) = (sqrtPriceBX96, sqrtPriceAX96);
-
+        //如果当前的price小于lower 说明price区间高于当前，则只需要token0,x
         if (sqrtPriceX96 <= sqrtPriceAX96) {
             liquidity = getLiquidityForAmount0(
                 sqrtPriceAX96,
                 sqrtPriceBX96,
                 amount0
             );
+            // 表面提供的price区间包含当前价格，则算入到当前流动性中
         } else if (sqrtPriceX96 <= sqrtPriceBX96) {
             uint128 liquidity0 = getLiquidityForAmount0(
                 sqrtPriceX96,
@@ -69,9 +71,10 @@ library LiquidityMath {
                 sqrtPriceX96,
                 amount1
             );
-
+            // 取最小值
             liquidity = liquidity0 < liquidity1 ? liquidity0 : liquidity1;
         } else {
+            // 说明当前的price高于price区间，则只需要token1,y
             liquidity = getLiquidityForAmount1(
                 sqrtPriceAX96,
                 sqrtPriceBX96,
