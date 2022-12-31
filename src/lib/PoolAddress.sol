29551058057cff093e369d29ab465575ba2b8fc4
1181406961 <1181406961@qq.com>
2022-12-31 15:32:34 +0800

    增加关于create2地址的说明

--- src/lib/PoolAddress.sol
+++ src/lib/PoolAddress.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.14;
 import "../UniswapV3Pool.sol";
 
 library PoolAddress {
+    // 模拟create2计算合约地址
     function computeAddress(
         address factory,
         address token0,
         address token1,
-        uint24 fee
+        uint24 tickSpacing
     ) internal pure returns (address pool) {
         require(token0 < token1);
-
+        // create2底层实现原理
         pool = address(
             uint160(
                 uint256(
@@ -19,7 +20,10 @@ library PoolAddress {
                         abi.encodePacked(
                             hex"ff",
                             factory,
-                            keccak256(abi.encodePacked(token0, token1, fee)),
+                            keccak256(
+                                abi.encodePacked(token0, token1, tickSpacing)
+                            ),
+                            // 加入createCode防止hash碰撞
                             keccak256(type(UniswapV3Pool).creationCode)
                         )
                     )
